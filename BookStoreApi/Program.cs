8caa16706555c77ed9470a5615cb8c7289495aad
museop <museop92@gmail.com>
2023-08-13 23:36:11 +0900

    style: :pencil2: fix swagger endpoint name

--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -23,7 +23,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStoreApi v1"));
 }
 
 app.UseHttpsRedirection();
