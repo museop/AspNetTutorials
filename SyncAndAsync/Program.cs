c181943ed5ef1771a2f8b90952a120ed6287b8d5
museop <museop92@gmail.com>
2023-10-03 22:18:21 +0900

    ✨feature: sync and async logic

---
+++ SyncAndAsync/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace SyncAndAsync;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Program Begin (Sync)");
+        DoAsSync();
+        Console.WriteLine("Program End (Sync)");
+        Console.ReadLine();
+
+        Console.WriteLine("Program Begin (Async)");
+        DoAsAsync();
+        Console.WriteLine("Program End (Async)");
+        Console.ReadLine();
+    }
+
+    public static void DoAsSync()
+    {
+        Console.WriteLine("1 - Starting");
+        var t = Task.Factory.StartNew(DoSomethingThatTaksTime);
+        Console.WriteLine("2 - Task started");
+        t.Wait();
+        Console.WriteLine("3 - Task completed with result: " + t.Result);
+    }
+
+    public static async void DoAsAsync()
+    {
+        Console.WriteLine("1 - Starting");
+        var t = Task.Factory.StartNew(DoSomethingThatTaksTime);
+        Console.WriteLine("2 - Task started");
+        var result = await t;
+        Console.WriteLine("3 - Task completed with result: " + t.Result);
+    }
+
+    public static int DoSomethingThatTaksTime()
+    {
+        Console.WriteLine("A - Started something");
+        Thread.Sleep(1000);
+        Console.WriteLine("B - Completed something");
+        return 123;
+    }
+}
